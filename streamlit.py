a331c7e1f27928ca6697381680c1d35271611054
reyhantampan <96065350+reyhantampan@users.noreply.github.com>
2024-08-06 23:30:18 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -308,10 +308,10 @@ elif options == "🔄 Preprocessing":
 
    # File uploader for CSV files
 uploaded_file = st.file_uploader("Pilih file CSV", type="csv")
-        if uploaded_file is not None:
-            data = pd.read_csv(uploaded_file)
-            st.write("Data yang Diunggah:")
-            st.write(data.head())
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write("Data yang Diunggah:")
+    st.write(data.head())
         
         # Pilih kolom untuk preprocessing
         column = st.selectbox("Pilih kolom untuk preprocessing", data.columns)
